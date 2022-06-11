56fe1f748efc9f9a2dc38414a22bf3754e63941c
Mengyang Liu <nikolaLiu@icloud.com>
2022-06-10 17:35:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from http import server
 
-server = server.HTTPServer(('localhost', 8080), server.SimpleHTTPRequestHandler)
+server = server.HTTPServer(('0.0.0.0', 8080), server.SimpleHTTPRequestHandler)
 server.serve_forever()
